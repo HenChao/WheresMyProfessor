1d603decc398820697643d835639016d6ceee293
HenChao <henchao@gmail.com>
2015-04-15 22:05:16 -0400

    Reorganized file structure. Removed code for terminal

---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var ws = new WebSocket("ws://" + window.location.hostname + ":5000/socket?Id=1");
+	
+	ws.onopen = function(){
+	}
+	ws.onmessage = function (evt) {
+	}
+	ws.onclose = function() {
+		ws.send("Closing socket connection");
+	}
+    
+    $( window ).konami({
+       code : [38,38,40,40,37,39,37,39], // up up down down left right left right
+       cheat: function() {
+           var img = $('#logo').show();
+           var width = img.get(0).width;
+           var screenWidth = "+=" + $(window).width();
+           var duration = 10000;
+           
+           img.css("left", -width).animate({
+               "left": screenWidth}, duration, function() {
+                    img.fadeOut(1000);
+           });
+           
+       }
+    });
+});
